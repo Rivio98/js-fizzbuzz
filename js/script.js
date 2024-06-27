61d039177fcc40a2456001208936a695b3216758
Rivio98 <rivaldogjoni1998@gmail.com>
2024-06-27 16:08:31 +0200

    [UPD] Added if for all the numbers they can be divided for 3 and 5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //Inizializzo un for che stampa numeri da 1 a 100 compreso e me li restituisce su console
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-}
+    //dentro al for aggiungo:
+
+    //se un numero diviso 3 e 5 ha come resto un numero 0 allora ridai FizzBuzz come valore
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz')
+    }
